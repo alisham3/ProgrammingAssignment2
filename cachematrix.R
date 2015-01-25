e5bd67e77c58d4224c8b2606cec60fa05198bb80
alisham3 <alishamanandhar@gmail.com>
2015-01-25 15:43:47 -0800

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function makes a matrix that can be inverted without issues.
+## Using the function prints the inverse. If already inverted, it will notify
+## the user and print the laready inverted matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix makes a matrix object that can cache itself. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  a<-NULL
+  set <function(y){
+  x <<- y
+  a <<- NULL
+  }
+  get<- function () x
+  set_inverse <- fucntion(solve) a <<- solve
+  get_inverse<- function() a 
+  list (set = set, get = get, 
+        set_inverse = set_inverse,
+        get_inverse = get_inverse)
 }
-
 
 ## Write a short comment describing this function
 
